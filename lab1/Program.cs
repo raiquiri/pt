e3b5b13f1b59b9841b0c79ea16779f695d308826
raiquiri <maks.fukanaga@mail.ru>
2024-03-04 00:33:05 +0500

    first commit

---
+++ lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList();
+
+            for (int i = 0; i < 20; i++)
+            {
+                array.add(i);
+            }
+            array.print();
+            array.insert(15, 3);
+            array.insert(20, 21);
+            array.print();
+            array[3] = 15;
+        }
+    }
+}
