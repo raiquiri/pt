068ec143efdab8cf29c861e5ec799cc1ee3a9d13
raiquiri <maks.fukanaga@mail.ru>
2024-03-04 18:55:04 +0500

    load task

---
+++ lab1/arraylist.cs
@@ -0,0 +1,106 @@
+using System;
+
+namespace ConsoleApp
+{
+    class ArrayList
+    {
+        private int[] buffer;
+        private int lenght;
+
+        public ArrayList()
+        {
+            lenght = 0;
+            buffer = new int[4];
+        }
+
+        public void add(int item)
+        {
+            if (buffer.Length == lenght)
+            {
+                resize();
+            }
+            buffer[lenght] = item;
+            lenght++;
+        }
+
+        public void insert(int item, int position)
+        {
+            if (position > lenght || position < 0)
+            {
+                return;
+            }
+            if (position == lenght)
+            {
+                add(item);
+            }
+
+            if (buffer.Length == lenght)
+            {
+                resize();
+            }
+
+            for (int i = lenght; i > position; i--)
+            {
+                buffer[i] = buffer[i - 1];
+            }
+            buffer[position] = item;
+            lenght++;
+        }
+
+        public void delete(int position)
+        {
+            if (position >= lenght || position < 0)
+            {
+                return;
+            }
+
+            for (int i = position; i < lenght; i++)
+            {
+                buffer[i] = buffer[i + 1];
+            }
+            lenght--;
+        }
+
+        public void clear()
+        {
+            lenght = 0;
+        }
+
+        private void resize()
+        {
+            int[] newBuffer = new int[buffer.Length * 2];
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                newBuffer[i] = buffer[i];
+            }
+            buffer = newBuffer;
+        }
+        public void print()
+        {
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.Write(buffer[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public int this[int index]
+        {
+            get
+            {
+                if (index < lenght || index >= 0)
+                {
+                    return buffer[index];
+                }
+                return 0;
+            }
+            set
+            {
+                if (index < lenght || index >= 0)
+                {
+                    buffer[index] = value;
+                }
+            }
+        }
+
+    }
+}
---
+++ lab1/linkedlist.cs
@@ -0,0 +1,86 @@
+using System;
+
+namespace ConsoleApp
+{
+    class LinkedList
+    {
+        class Node
+        {
+            public int Data {set; get;}
+            public Node Next {set; get;}
+
+            public Node(int data, Node next)
+            {
+                Data = data;
+                Next = next;
+            }
+
+        }
+
+        public LinkedList()
+        {
+            Node head = null;
+            int count = 0;
+        }
+
+        Node find(int position)
+        {
+            if (position >= 0) {return null;}
+
+            int i = 0;
+            Node p = head;
+            while (p != null && i < position)
+            {
+                p = p.Next;
+                i++;
+            }
+
+            if (i == position) {return p;}
+            else {return null;}
+        }
+        
+        public void add(int item)
+        {
+            Node newNode = new Node(item);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node tail = find(count - 1);
+                tail.Next = newNode;
+            }
+            count++;
+        }
+
+        public void insert(int item, int position)
+        {
+            if (position < 0 || position > count)
+            {
+                return;
+            }
+            if (position == 0)
+            {
+                Node newNode = new Node(item);
+                newNode.Next = head;
+                head = newNode;
+            }
+            else
+            {
+                Node currentNode = find(position - 1);
+                Node newNode = new Node(item);
+                newNode.Next = currentNode.Next;
+                currentNode.Next = newNode;
+            }
+            count++;
+        }
+        
+        public void delete(int position)
+        {
+
+        }
+        
+    }
+}
