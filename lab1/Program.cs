f55bee9849f1cb680576cbf223187fa5e50164ae
raiquiri <142206033+raiquiri@users.noreply.github.com>
2024-03-19 13:10:57 +0500

    Update Program.cs
    load lab2

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,16 +1,47 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp
-{
+{ 
     class Program
     {
         public static void Main(string[] args)
         {
-            ArrayList arrayList = new ArrayList();
+            BaseList arrayList = new ArrayList();
             LinkedList linkedList = new LinkedList();
             Random random = new Random();
 
-            for (int i = 0; i <= 5000; i++)
+            BaseList testList = new ArrayList();
+
+            arrayList.add(8);
+            arrayList.add(5);
+            arrayList.add(10);
+            arrayList.add(13);
+            arrayList.add(1);
+            arrayList.add(2);
+            arrayList.add(3);
+            arrayList.add(4);
+            arrayList.add(11231230);
+            arrayList.add(12333);
+
+            arrayList.print();
+            arrayList.sort();
+            arrayList.print();
+
+
+            testList.assign(arrayList);
+            testList.print();
+
+            Console.WriteLine(arrayList.equals(testList));
+
+
+
+
+
+
+
+
+
+            /*for (int i = 0; i <= 5000; i++)
             {
                 int factor = random.Next(1, 5);
                 int number = random.Next(0, 1000);
@@ -30,11 +61,11 @@ namespace ConsoleApp
                         arrayList.insert(number, index);
                         linkedList.insert(number, index);
                         break;
-                        /*case 4:
-                            arrayList.clear();
-                            linkedList.clear();
-                            break;*/
-                }
+                    /*case 4:
+                        arrayList.clear();
+                        linkedList.clear();
+                        break;
+                }   
             }
 
             for (int i = 0; i < arrayList.Count(); i++)
@@ -47,7 +78,7 @@ namespace ConsoleApp
             }
 
 
-            Console.WriteLine("accept " + linkedList[0] + " " + arrayList[0]);
+            Console.WriteLine("accept " + linkedList[0] + " " + arrayList[0]);*/
         }
     }
 }
